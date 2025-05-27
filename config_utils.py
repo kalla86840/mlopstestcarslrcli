16ccefe004be47d2724ac12518bec5f39b9bd089
kalla86840 <122142035+kalla86840@users.noreply.github.com>
2025-05-27 00:38:49 -0700

    Delete config_utils.py

--- config_utils.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-def load_config(path="config.json"):
-    script_dir = os.path.dirname(__file__)
-    config_path = os.path.join(script_dir, path)
-    with open(config_path, "r") as f:
-        return json.load(f)
